6ac0cee47a4bf447ee0de6ee59242bf1fcfc0378
Ryan M Sullivan <rmmichael95@gmail.com>
2026-05-14 20:19:45 -0700

    Adjusted cpp

--- lua/snippets/cpp.lua
+++ lua/snippets/cpp.lua
@@ -13,8 +13,6 @@ local t = ls.text_node
 local i = ls.insert_node
 local f = ls.function_node
 local c = ls.choice_node
-local d = ls.dynamic_node
-local sn = ls.snippet_node
 
 -- ── helpers ──────────────────────────────────────────────────────────────────
 
